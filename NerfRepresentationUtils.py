598aaa0e23adbfc3a592f62b6d81516a143c4e18
EnegoComley <enegocomley@gmail.com>
2026-05-12 16:04:30 +0100

    Fixed stupid bug.

--- NerfRepresentationUtils.py
+++ NerfRepresentationUtils.py
@@ -214,7 +214,7 @@ class GridRepresentationEncoder(nn.Module):
     def forward(self, representation):
         batch_size, n_patches, latent_size = representation.shape
 
-        x = n_patches.view(batch_size * n_patches, -1)
+        x = representation.view(batch_size * n_patches, -1)
 
         x = self.patch_encoder(x)
         x = x.view(batch_size, n_patches, -1)

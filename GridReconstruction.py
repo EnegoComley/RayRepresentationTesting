Hopefully fixed bug with the res net and gradients.

GridReconstruction.py
@@ -43,7 +43,7 @@ class ResnetBlock3D(nn.Module):
         out = self.conv1(x)
         out = self.bn1(out)
         out = self.relu(out)
-        out += residual
+        out = residual + out
         out = self.bn2(out)
         out = self.relu2(out)
         return out
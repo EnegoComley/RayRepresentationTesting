15336315cf9d81fac827011517aefd337f65b96f
EnegoComley <enegocomley@gmail.com>
2026-07-30 13:36:46 +0100

    Fixed a bug with the learning rate scheduler.

--- InterpolatableGridReconstruction.py
+++ InterpolatableGridReconstruction.py
@@ -250,7 +250,7 @@ class InterpolatableGridReconstruction(L.LightningModule):
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
         scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.5, patience=5)
-        return {"optimizer": optimizer, "lr_scheduler": scheduler, "monitor": "val_total_loss"}
+        return {"optimizer": optimizer, "lr_scheduler": scheduler, "monitor": "val_rmse_loss"}
 
 
 

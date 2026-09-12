780bddf503ab83a80082117eb5bea47548109b48
EnegoComley <enegocomley@gmail.com>
2026-09-12 22:28:04 +0100

    Fixed a bug with the test rgba script when there are not many checkpoints.

--- TestRGBAReconstructionModels.py
+++ TestRGBAReconstructionModels.py
@@ -28,14 +28,15 @@ model_folders = [x for x in os.listdir(checkpoints_folder) if os.path.isdir(os.p
 for model_folder_name in model_folders:
     model_folder_path = os.path.join(checkpoints_folder, model_folder_name)
     checkpoints = [x for x in os.listdir(model_folder_path) if os.path.isfile(os.path.join(model_folder_path, x))]
-    renamed_checkpoints = [x.replace("=", ".") for x in checkpoints]
-    renamed_checkpoints = [x.replace("_", ".") for x in renamed_checkpoints]
-    renamed_checkpoints = [x.replace("-", ".") for x in renamed_checkpoints]
-    epochs = np.array([int(x.split(".")[1]) for x in renamed_checkpoints])
-    checkpoint_name = checkpoints[np.argmax(epochs)]
-    model = RGBAGridReconstruction.load_from_checkpoint(os.path.join(model_folder_path, checkpoint_name), device=torch.device("cuda"))
-    model.test_output_dir = f"RGBAGridReconstructionTestOutputs/{model_folder_name}/"
-    results = trainer.test(model, datamodule=dataset_loader)
+    if len(checkpoints) > 5:
+        renamed_checkpoints = [x.replace("=", ".") for x in checkpoints]
+        renamed_checkpoints = [x.replace("_", ".") for x in renamed_checkpoints]
+        renamed_checkpoints = [x.replace("-", ".") for x in renamed_checkpoints]
+        epochs = np.array([int(x.split(".")[1]) for x in renamed_checkpoints])
+        checkpoint_name = checkpoints[np.argmax(epochs)]
+        model = RGBAGridReconstruction.load_from_checkpoint(os.path.join(model_folder_path, checkpoint_name), device=torch.device("cuda"))
+        model.test_output_dir = f"RGBAGridReconstructionTestOutputs/{model_folder_name}/"
+        results = trainer.test(model, datamodule=dataset_loader)
 
-    with open(f"RGBAGridReconstructionTestOutputs/{model_folder_name}/test_results.json", "w") as f:
-        json.dump(results, f, indent=4)
+        with open(f"RGBAGridReconstructionTestOutputs/{model_folder_name}/test_results.json", "w") as f:
+            json.dump(results, f, indent=4)
